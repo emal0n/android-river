1af88ab157f813bee1eb782625ba2eb7255230b0
Vhmit <viktor.9630@protonmail.com>
2024-07-01 10:41:14 -0300

    river: DerpFest-ify 14
    Change-Id: Ie3c225b35f1164e93ce0bf2598cb0d333c0ebfbb
    Co-authored-by: emalon91 <edmundoneto511@hotmail.com>

--- derp_river.mk
+++ derp_river.mk
@@ -1,17 +1,23 @@
 # Inherit some common Lineage stuff
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+DERP_BUILDTYPE := Official
+USE_LEGACY_BOOTANIMATION := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_USES_PICO_GAPPS := true
 
 #BUILD_FINGERPRINT := motorola/river_retail/river:10/QPUS30.52-33-11/df129:user/release-keys
 PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := river
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_MODEL := moto g(7)
-PRODUCT_NAME := lineage_river
+PRODUCT_NAME := derp_river
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
         PRODUCT_NAME=river \
